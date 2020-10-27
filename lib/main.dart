d45a378754b3a301ac681ba3edc0ac6d00df57cf
ramcalm <ram.callm@gmail.com>
2020-10-27 12:52:49 +0530

    Final Parking Bud App

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:parkbud/models/place.dart';
+import 'package:parkbud/screens/search.dart';
+import 'package:parkbud/services/geolocator_service.dart';
+import 'package:parkbud/services/places_service.dart';
+import 'package:provider/provider.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  final locatorService = GeoLocatorService();
+  final placesService = PlacesService();
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        FutureProvider(create: (context) => locatorService.getLocation()),
+        ProxyProvider<Position, Future<List<Place>>>(
+          update: (context, position, places) {
+            return (position != null)
+                ? placesService.getPlaces(position.latitude, position.longitude)
+                : null;
+          },
+        )
+      ],
+      child: MaterialApp(
+        title: 'Parking App',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: Search(),
+      ),
+    );
+  }
+}
